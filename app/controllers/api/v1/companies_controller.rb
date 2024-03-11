aab3699bd3e2e6b0f93c597ce8cf54c6e6c649f2
nachoabad <liao2512@gmail.com>
2024-03-11 21:06:56 +0100

    Add missing id attribute on companies index json

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::CompaniesController < Api::V1::BaseController
     pagination, companies = pagy(companies, items: params[:items] || 2)
 
     render json: {
-      companies: companies.as_json(only: [:name, :industry, :employee_count], methods: [:deals_amounts_sum]),
+      companies: companies.as_json(only: [:id, :name, :industry, :employee_count], methods: [:deals_amounts_sum]),
       pagination: pagy_metadata(pagination)
     }
   end
